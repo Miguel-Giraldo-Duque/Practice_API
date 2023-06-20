21ca476c9204b1127af02d785e053166f6f68522
Miguel Angel <miguelgiradu@gmail.com>
2023-06-20 15:40:03 -0500

    update url server

--- client/script.js
+++ client/script.js
@@ -90,7 +90,7 @@ const handleSumbit = async (e) =>{
 
 
 
-  const response =  await fetch("http://localhost:5000/" ,{
+  const response =  await fetch("https://trustify.onrender.com" ,{
     method: "POST",
     headers:{
       "Content-Type" : "application/json"
